5678884a2547c624aceabf74f95f3c28fd3ce459
MD Yousuf Jamil <75928417+yousufj960@users.noreply.github.com>
2021-12-27 12:27:51 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+
+
+
+
+    $('.js-wp-2').waypoint(function(direction) {
+
+        $('.js-wp-2').addClass('animated slideInUp');
+
+    }, {
+        offset: '70%'
+    });
+    
+   
+
+
+
+
+});
